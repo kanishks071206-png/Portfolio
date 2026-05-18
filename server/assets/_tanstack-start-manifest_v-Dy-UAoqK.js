623a322205f746b97e7c79c1f9e778990b148e8b
hemel <hemel@0xDEADVrm.0xDEADVrm>
2026-05-18 20:00:00 +0530

    Updates

--- server/assets/_tanstack-start-manifest_v-Dy-UAoqK.js
+++
@@ -1,4 +0,0 @@
-const tsrStartManifest = () => ({ routes: { __root__: { filePath: "/home/hemel/Downloads/kanishk07-main/src/routes/__root.tsx", children: ["/"], assets: void 0, preloads: ["/assets/index-ukP0MKE0.js"] }, "/": { filePath: "/home/hemel/Downloads/kanishk07-main/src/routes/index.tsx", children: void 0, assets: void 0, preloads: ["/assets/index-15Ssh2J2.js"] } }, clientEntry: "/assets/index-ukP0MKE0.js" });
-export {
-  tsrStartManifest
-};
--- server/index.js
+++
@@ -1,5 +0,0 @@
-import { w } from "./assets/worker-entry-BxlaVkpi.js";
-import "node:events";
-export {
-  w as default
-};
